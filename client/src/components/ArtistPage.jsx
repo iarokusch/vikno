4bb9e60c410d4dacd9a020f684241babe5b31a9d
ilica <ilikayaroslav@gmail.com>
2023-06-16 14:18:31 +0200

    26 commit

--- client/src/components/ArtistPage.jsx
+++ client/src/components/ArtistPage.jsx
@@ -145,7 +145,7 @@ const ArtistPage = () => {
                                     <NavLink
                                         to='/changeitemdata'
                                         state={item}
-                                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2'
+                                        className='bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2'
                                     >
                                         Edit
                                     </NavLink>
